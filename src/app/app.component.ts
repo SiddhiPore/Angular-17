8ddd71326cca1d67711592e193a40eeff5e4af07
Siddhi Pore <siddhipore16@gmail.com>
2024-01-07 11:33:54 +0530

    Basics of Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,79 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { UserModuleModule } from './user-module/user-module.module';
+import { LoginComponent } from './user-module/login/login.component';
+import { count } from 'console';
+
+  
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UserComponent, UserModuleModule, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'basic';
+
+  getName(Name:string)
+  {
+    alert(Name)
+  }
+  getNumber(Num:number)
+  {
+    console.log(Num)
+  }
+
+  getData(val:string)
+  {
+    console.warn(val)
+  }
+  displayVal=''
+  getValue(val1: string)
+  {
+    console.warn(val1)
+    this.displayVal=val1
+  }
+ count=0
+  getCount(type:string)
+  {
+    type==='minus'? this.count-- : this.count++
+  }
+
+
+  isDrivelicen= false
+
+  color='black';
+
+  selection=10;
+  
+  // getSwitch(val3: string)
+  // {
+  //   console.warn(val3)
+  // }
+  items=['Angular', 'Java', 'Cpp','Python'];
+
+  courses = [
+    { id: 1, name: "Angular For Beginners" },
+    { id: 2, name: "Angular Core Deep Dive" },
+    { id: 3, name: "Angular Forms In Depth" },
+  ];
+
+  candidates = [ 
+    {name: 'Siddhi', cont: '9146', email: 'ss@gmail.com' },
+      {name: 'Vedika', cont: '3456', email: 'dd@gmail.com' },
+      {name: 'Shraddha', cont: '8765', email: 'sp@gmail.com' },
+    
+    ];
+
+    studentdetail = [
+      {name:'Prathmesh' , age:'68',social:['Insta','facebook']},
+      {name:'Vikas' , age:'67',social:['Insta','Linedin']},
+      {name:'Aniket' , age:'69',social:['LinkedIn','facebook']},
+      {name:'Vivek' , age:'58',social:['Insta','facebook']},
+    ]
 }
+
