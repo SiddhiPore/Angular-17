a65904af5893bf2b085dac853ce275e56cadc454
Siddhi Pore <siddhipore16@gmail.com>
2024-01-11 22:20:56 +0530

    To do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MatButtonModule, MatDividerModule, MatIconModule,MatCardModule,MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'advance';
+
+  taskdetail:any[] = [];
+
+
+  addtask(item:string)
+  {
+    this.taskdetail.push({id:this.taskdetail.length,name:item})
+
+  }
+
+  Remove(id:number)
+  {
+    this.taskdetail = this.taskdetail.filter(i => i.id!==id);
+  }
 }
