4b3da11dcb27a070fd37c5cf6c9c12fd4887d954
Siddhi Pore <siddhipore16@gmail.com>
2024-01-17 15:28:30 +0530

    Dynamic Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     {
-        path : 'user',
+        path : 'user/:id',
         component: UserComponent // we can also give blank path to show as front page
     },
 
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
 
+  constructor(private route: ActivatedRoute){}
+ userId:any;
+ ngOnInit(): void {
+   console.warn("user id is ", this.route.snapshot.paramMap.get('id'));
+   this.userId =this.route.snapshot.paramMap.get('id');
+ }
 }
